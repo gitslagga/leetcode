54216720f4bcffb837f27d0866d899a549b5edfe
slagga <957766762@qq.com>
2023-06-02 22:54:44 +0800

    feat: add remove-duplicates-from-sorted-array solution and test

---
+++ 26.remove-duplicates-from-sorted-array/26.remove-duplicates-from-sorted-array_test.go
@@ -0,0 +1,21 @@
+package solution
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseKGroup(t *testing.T) {
+	input := [][]int{
+		{1, 1, 2},
+		{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+	}
+	expectResult := []int{2, 5}
+
+	for k, v := range input {
+		result := RemoveDuplicates(v)
+		if reflect.DeepEqual(result, expectResult[k]) == false {
+			t.Errorf("return wrong result, expect:%v, got:%v", expectResult[k], result)
+		}
+	}
+}

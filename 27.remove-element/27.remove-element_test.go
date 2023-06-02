fcf0c3a5e6dda02386fcdd26cf65ad39413c63a7
slagga <957766762@qq.com>
2023-06-02 23:07:11 +0800

    feat: add remove-element solution and test

---
+++ 27.remove-element/27.remove-element_test.go
@@ -0,0 +1,24 @@
+package solution
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseKGroup(t *testing.T) {
+	input := []struct {
+		nums []int
+		val  int
+	}{
+		{[]int{3, 2, 2, 3}, 3},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2},
+	}
+	expectResult := []int{2, 5}
+
+	for k, v := range input {
+		result := RemoveElement(v.nums, v.val)
+		if reflect.DeepEqual(result, expectResult[k]) == false {
+			t.Errorf("return wrong result, expect:%v, got:%v", expectResult[k], result)
+		}
+	}
+}
